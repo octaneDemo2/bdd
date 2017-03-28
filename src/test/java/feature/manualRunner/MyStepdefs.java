2d6b76893feb8837921a3a82b58c38e681fe809d
yuval <yuval.koren@hpe.com>
2017-03-28 14:53:47 +0300

    another change

--- src/test/java/feature/manualRunner/MyStepdefs.java
+++ src/test/java/feature/manualRunner/MyStepdefs.java
@@ -60,6 +60,7 @@ public class MyStepdefs {
 
 
 
+
     @Then("^check it again")
     public void checkIt() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
